94181f48ce4d64ae3d4e813189da19f8279473d2
Tharun-coder <tharunece95@gmail.com>
2021-08-22 18:07:08 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const notes = require("./data/notes");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const notesRoutes = require("./routes/notesRoutes");
+const { notFound, errorHandler } = require("./middlewares/error.Middleware");
+const path = require("path");
 
 dotenv.config();
 const app = express();
+app.use(cors());
+app.use(express.json());
 const PORT = process.env.PORT || 5000;
+connectDB();
 
-app.get("/", async (req, res) => {
-  res.send("Hi Im Node server");
-});
+app.use("/api/users", userRoutes);
+app.use("/api/notes", notesRoutes);
 
-app.get("/notes", async (req, res) => {
-  try {
-    res.json(notes);
-  } catch (err) {
-    res.json({
-      message: err,
-    });
-  }
-});
+// Deployment
 
-app.get("/notes/:id", async (req, res) => {
-  try {
-    const note = notes.find((n) => n._id === req.params.id);
-    res.json(note);
-  } catch (err) {
-    res.json({
-      message: err,
-    });
-  }
-});
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server started on PORT - ${PORT}`));
